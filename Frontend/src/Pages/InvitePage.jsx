9e242607e3eb81f386fefa39d0f4402d8a95d6ac
Siddhant Shah <siddhantkumar632@gmail.com>
2025-10-27 17:08:24 +0530

    fix

--- Frontend/src/Pages/InvitePage.jsx
+++ Frontend/src/Pages/InvitePage.jsx
@@ -27,16 +27,16 @@ function InvitePage() {
           localStorage.setItem("invitetoken", invitetoken);
           navigate(`/?invitetoken=${invitetoken}`); //redirectin to login
         } else {
-          navigate("/dashboard")
-          const data =await res.json();
-          
-          
+          const data = await res.json();
           setInviteData(data.invite);
-              if (data.success) {
-                // navigate(-1);
-                toast.success(data.message);
-              } else toast.error(data.message);
-          // console.log("added");
+
+          if (data.success) {
+            toast.success(data.message);
+            navigate("/dashboard");
+          } else {
+            toast.error(data.message);
+            navigate("/");
+          }
         }
       } catch (err) {
         console.error(err);
@@ -46,40 +46,7 @@ function InvitePage() {
       checkInvite();
     }
   }, [invitetoken]);
-  return (
-    <>
-      <div className=" w-[80vw] bg-blue-30 flex flex-col justify-start md:items-center text-white">
-        {inviteData && (
-          <div className="md:hidden h-[20vh] w-[90vw] md:h-[30vh] md:w-80 bg-gradient-to-r from-blue-500 to-blue-200 rounded-xl flex flex-col justify-center items-start p-3 md:px-8 gap-1 md:gap-2 m-2 ml-3">
-            <h2 className="font-bold text-xl md:text-2xl">
-              Name : {inviteData?.name}
-            </h2>
-            <h3 className="md:text-l">Message : {inviteData?.message}</h3>
-            <div className="w-[80vw] md:w-80 bg-red-60 flex gap-5">
-              <h3 className="md:text-l">
-                <span>E-mail : </span>
-                <span className="h-2 bg-red-400 px-4 md:px-2 py-[.2vw] ml-3 rounded-md">
-                  inviteData?.email
-                </span>
-              </h3>
-              <h3 className="md:text-l">
-                <span>Role : </span>
-                <span className="h-2 bg-red-400 px-4 md:px-2 py-[.2vw] ml-3 rounded-md">
-                  inviteData?.role
-                </span>
-              </h3>
-            </div>
-            <h3 className="md:text-l">Invited by : {inviteData?.invitedBy}</h3>
-            <Link to={`/dashboard`}>
-              <button className="h-[5vh] w-[8vw] bg-red-300 rounded-xl ">
-                Accept invitation
-              </button>
-            </Link>
-          </div>
-        )}
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default InvitePage;
--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { useState } from "react";
 import { useUser } from "../contextApi/UserContext";
 import { toast } from "react-toastify";
-import { Link,useNavigate,useSearchParams } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 
 function Login() {
-  const {login} = useUser()
-  const navigate=useNavigate();
+  const { login } = useUser();
+  const navigate = useNavigate();
   const [searchparams] = useSearchParams();
-  const invitetoken = searchparams.get("invitetoken")
+  const invitetoken = searchparams.get("invitetoken");
 
   const [formData, setFormData] = useState({
     email: "",
@@ -19,96 +19,95 @@ function Login() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
- console.log("Form data :", formData);
- try{
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/users/login`,{
-           method:"POST",
-           credentials:"include",
-          headers:{
-            "content-type":"application/json"
-          },
-          body: JSON.stringify(formData)
-           
- }) ;
+    console.log("Form data :", formData);
+    try {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/users/login`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
- const data = await res.json();
- console.log(data);
+      const data = await res.json();
+      console.log(data);
 
-
- if(data.success){
-  login()
-  toast.success(data.message)
-if (invitetoken) {
-            console.log("3 - Redirecting to Invite Page");
-            navigate(`/invitepage/${invitetoken}`);
-        } 
+      if (data.success) {
+        login();
+        toast.success(data.message);
+        if (invitetoken) {
+          console.log("3 - Redirecting to Invite Page");
+          navigate(`/invitepage/${invitetoken}`);
+        }
         // 2. Check for a valid company ID (Dashboard access)
         else if (data.companyId) {
-            console.log("1 - Redirecting to Dashboard");
-            navigate("/dashboard");
-        } 
-        // 3. Fallback: If no invite and no company ID, send to create company
-        else { 
-            console.log("2 - Redirecting to Add Company");
-            navigate("/addcompany");
+          console.log("1 - Redirecting to Dashboard");
+          navigate("/dashboard");
         }
-  
- }else(
-   toast.error(data.message)
- )
- 
- }catch(err){
-
-  console.log(err);
-  
- }
- 
- 
+        // 3. Fallback: If no invite and no company ID, send to create company
+        else {
+          console.log("2 - Redirecting to Add Company");
+          navigate("/addcompany");
+        }
+      } else toast.error(data.message);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <>
-    <div className="h-[90vh] md:h-[80vh]  flex  justify-center items-center">
-    <div className="flex flex-col justify-center items-center   w-[80vw] md:w-[30vw]  rounded p-5 border bg-slate-800 border-cyan-400 shadow-lg shadow-cyan-400/30">
-      <form
-        action=""
-        onSubmit={handleSubmit}
-        className="flex flex-col justify-center items-center gap-3"
-      >
-        <h1 className="text-2xl font-bold ">Login</h1>
-        <input
-          type="text"
-          placeholder="User email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          className="border border-cyan-300 rounded p-2 mb-2  w-[60vw] md:w-[25vw]"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          className="border border-cyan-300 rounded p-2 mb-2  w-[60vw] md:w-[25vw]"
-        />
-        <Link to="/forgot-password" className="text-cyan-500 mb-1  w-[60vw] md:w-[25vw]">
-          Forgot Password?
-        </Link>
-        <button type="submit" className="hover:bg-cyan-400 text-white p-2  w-[60vw] md:w-[25vw] rounded border bg-cyan-950 border-cyan-400 shadow-lg shadow-cyan-400/30">
-          Login
-        </button>
-        <p className="text-gray-500 mb-4  w-[60vw] md:w-[25vw]">
-          Don't have an account?
-          <Link to={`/register/?invitetoken=${invitetoken}`} className="text-cyan-400  ">
-            Sign Up
-          </Link>
-        </p>
-      </form>
-    </div>
-    </div>
+      <div className="h-[90vh] md:h-[80vh]  flex  justify-center items-center">
+        <div className="flex flex-col justify-center items-center   w-[80vw] md:w-[30vw]  rounded p-5 border bg-slate-800 border-cyan-400 shadow-lg shadow-cyan-400/30">
+          <form
+            action=""
+            onSubmit={handleSubmit}
+            className="flex flex-col justify-center items-center gap-3"
+          >
+            <h1 className="text-2xl font-bold ">Login</h1>
+            <input
+              type="text"
+              placeholder="User email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="border border-cyan-300 rounded p-2 mb-2  w-[60vw] md:w-[25vw]"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="border border-cyan-300 rounded p-2 mb-2  w-[60vw] md:w-[25vw]"
+            />
+            <Link
+              to="/forgot-password"
+              className="text-cyan-500 mb-1  w-[60vw] md:w-[25vw]"
+            >
+              Forgot Password?
+            </Link>
+            <button
+              type="submit"
+              className="hover:bg-cyan-400 text-white p-2  w-[60vw] md:w-[25vw] rounded border bg-cyan-950 border-cyan-400 shadow-lg shadow-cyan-400/30"
+            >
+              Login
+            </button>
+            <p className="text-gray-500 mb-4  w-[60vw] md:w-[25vw]">
+              Don't have an account?
+              <Link
+                to={`/register/?invitetoken=${invitetoken}`}
+                className="text-cyan-400  "
+              >
+                Sign Up
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
